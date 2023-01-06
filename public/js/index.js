9f2ba68ee76088a8470ed1d1741e6ed86f4c614e
sebastian Reyes <sebreyes222gmail.com>
2023-01-06 14:24:40 +0100

    ajout import export guerrier

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
 import * as CLASS from "./module/boss.js"
 let Lilith = new CLASS.Boss('Lilith', 100, 100)
 console.log(Lilith,CLASS.Soron, CLASS.Chronos);
+import * as GUERRIER from "./module/guerrier.js";
---
+++ public/js/module/guerrier.js
@@ -0,0 +1,8 @@
+export class Guerrier {
+    constructor(nom, PointsDeVie, PointDattaque, PointDeRage){
+    this.nom =nom
+    this.PointsDeVie = PointsDeVie
+    this.PointDattaque = PointDattaque
+    this.PointDeRage = PointDeRage
+    }
+}
