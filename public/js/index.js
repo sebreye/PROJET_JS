b9a90bef760d25852dfc97ca2ccd23049b23f2ce
sebastian Reyes <sebreyes222gmail.com>
2023-01-06 14:40:54 +0100

    ajout import export mage

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,10 @@
 import * as CLASS from "./module/boss.js"
 let Lilith = new CLASS.Boss('Lilith', 100, 100)
 console.log(Lilith,CLASS.Soron, CLASS.Chronos);
+
 import * as GUERRIER from "./module/guerrier.js";
+let guerrier = new GUERRIER.Guerrier('', 0, 0, 0)
+console.log(guerrier);
+
+import * as MAGE from "./module/mage.js";
+let mage = new MAGE.Mage('',0, 0)
---
+++ public/js/module/mage.js
@@ -0,0 +1,8 @@
+export class Mage {
+    constructor(nom, PointsDeVie, PointsDattaque, PointsDeMana){
+        this.nom = nom
+        this.PointsDeVie = PointsDeVie
+        this.PointsDattaque = PointsDattaque
+        this.PointsDeMana = PointsDeMana
+    }
+}
